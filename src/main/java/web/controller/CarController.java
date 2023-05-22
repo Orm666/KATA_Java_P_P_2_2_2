b79821d3fc533c5382905dc7a7b2648cbd30fbc8
Orm666 <anton.ga4an@yandex.ru>
2023-05-22 10:42:08 +0300

    1) Убрал заполнение списка машин из CarController; 2) Добавил конструктор в CarDaoImp с заполнением списка машин; 3) Удалил HelloController.java и index.html.

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import web.model.Car;
 import web.service.CarService;
 import web.service.CarServiceImp;
 
@@ -16,12 +15,6 @@ public class CarController {
     public String printCars(@RequestParam(value = "count",defaultValue = "5") int carCount, ModelMap model) {
 
         CarService carService = new CarServiceImp();
-
-        carService.add(new Car("Car1", "RED", 1));
-        carService.add(new Car("Car2", "BLUE", 2));
-        carService.add(new Car("Car3", "GREEN", 3));
-        carService.add(new Car("Car4", "ORANGE", 4));
-        carService.add(new Car("Car5", "ROSE", 5));
 
         model.addAttribute("cars", carService.listCountCars(carCount > 5 ? 5 : carCount));
 
--- src/main/java/web/dao/CarDaoImp.java
+++ src/main/java/web/dao/CarDaoImp.java
@@ -12,6 +12,14 @@ public class CarDaoImp implements CarDao {
 
     private final List<Car> cars = new ArrayList<>();
 
+    public CarDaoImp() {
+        cars.add(new Car("Car1", "RED", 1));
+        cars.add(new Car("Car2", "BLUE", 2));
+        cars.add(new Car("Car3", "GREEN", 3));
+        cars.add(new Car("Car4", "ORANGE", 4));
+        cars.add(new Car("Car5", "ROSE", 5));
+    }
+
     @Override
     public void add(Car car) {
         cars.add(car);
